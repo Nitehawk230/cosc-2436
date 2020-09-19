27b6336adcb83c5dce9ebd87409d841b3c087938
Nitehawk230 <31257886+Nitehawk230@users.noreply.github.com>
2020-09-19 18:12:01 -0500

    Started ValidFlojure PS2

---
+++ PS2/PS2-ValidFlojure/ValidFlojureCA.cpp
@@ -0,0 +1,36 @@
+/******************************
+   Colton Anderson
+   9/19/2020
+   ValidFlojureCA.cpp
+   Write a program to validate code from a test file to ensure
+   every (,{,[,],},) has its correlating partner.
+********************************/
+
+// Headers
+#include <iostream>
+#include <cstdlib>
+#include <string>
+using namespace std;
+
+// Global variables
+
+// Function declarations
+
+int main()
+{
+	//	Your code goes here
+    
+    
+	//	Make sure we place the end message on a new line
+    cout << endl;
+
+	//	The following is system dependent.  It will only work on Windows
+    // system("PAUSE");
+
+	/* 
+	// A non-system dependent method is below
+	cout << "Press any key to continue";
+	cin.get();
+	*/
+    return 0;
+}
